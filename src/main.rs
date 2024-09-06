cb53a5d136a44741725c78baa9fd2e0a5dbb294f
Julind Mara <marajulind@outlook.com>
2024-09-06 02:12:12 +0200

    Move to tree-sitter based parsing
     + Chumsky as good as it was, was missing critical information regarding
       line and position of the character, which made it difficult to
       approach it in terms for lsp support.
     + This is a new start for the project effectively to use tree-sitter as
       the parser generator for the language

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-mod parsing;
-mod repr;
-
-use std::env;
-
-use crate::{parsing::rules_parser::parse, repr::representations::Service};
-
 pub fn main() {
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(20)
-        .build_global()
-        .unwrap();
-
-    let path: String = env::args().nth(1).expect("A path is needed");
-
-    println!("Running with: {:?}", path);
-
-    let src = std::fs::read_to_string(path).unwrap();
-
-    let stream = Vec::from_iter(src.chars());
-
-    let ast = parse(stream, false);
-
-    match ast {
-        Ok(tree) => {
-            let repr = Service::generate_from_service_definition(tree);
-            print!("{:#?}", repr.get_variables());
-        }
-        Err(errs) => print!("{:#?}", errs),
-    }
+  print!("Hello");
 }
--- src/parsing/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod models;
-pub mod rules_parser;
-
-mod errors;
-
-mod expression_parser;
-mod helper_parsers;
